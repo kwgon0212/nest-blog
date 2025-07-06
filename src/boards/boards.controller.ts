a2999cf63c075e59a2879202398cdf5a463ee980
kwgon0212 <kwgon0102@gmail.com>
2025-07-06 15:59:37 +0900

    feat: pipe를 이용한 title, desc가 빈문자열 일시 에러처리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
@@ -32,6 +34,7 @@ export class BoardsController {
   }
 
   @Post('/')
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
